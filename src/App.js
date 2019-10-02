1fad75cb5d8cb26cd11b47903279728db27b0f10
duc <nguyenminhduc1327@gmail.com>
2019-10-02 15:48:41 +0700

    COMMIT full code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 import { Row } from 'react-bootstrap';
 import './App.css';
-//component
+import { connect } from 'react-redux';
+import {handleChange} from './actions/promoCodeActions';
+//component 
 import Subtotal from './components/subtotal/subtotal';
 import PickupSavings from './components/PickupSavings/PickupSavings';
 import TaxesFees from './components/TaxesFees/TaxesFees';
 import EstimatedTotal from './components/EstimatdeTotal/EstimatedTotal';
 import ItemDetails from './components/ItemDetails/ItemDetails';
+import PromoCodeDiscount from './components/PromoCode/PromoCode';
+
 
 class App extends Component {
 
   constructor(props, context) {
     super(props, context);
     this.state = {
-      total: 100,
+      total: 200,
       PickupSavings: -3.85,
       taxes: 0,
-      estimatedTotal: 0
+      estimatedTotal: 0,
+      disablePromoButton: false
     }
   }
   
+  componentDidMount = () => {
+    this.setState({
+      taxes: (this.state.total + this.state.PickupSavings) * 0.0875
+    }, () => {
+      this.setState({
+        estimatedTotal: this.state.total + this.state.PickupSavings + this.state.taxes
+      })
+    })
+  }
+
+  giveDiscountHandler = () => {
+    if(this.props.promoCode === 'DISCOUNT'){
+      this.setState({
+        estimatedTotal: this.state.estimatedTotal * 0.9
+      }, () =>{
+        this.setState({
+          disablePromoButton: true
+        })
+      })
+    }
+  }
 
   render() {
     return (
@@ -30,7 +56,12 @@ class App extends Component {
           <TaxesFees taxes={this.state.taxes.toFixed(2)} />
           <hr />
           <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
-          <ItemDetails />
+          <ItemDetails price={this.state.estimatedTotal.toFixed(2)}/>
+          <hr />
+          <PromoCodeDiscount 
+            giveDiscount={() => this.giveDiscountHandler()}
+            isDisabled={this.state.disablePromoButton}
+          />
         </div>
       </div>
     );
@@ -38,4 +69,8 @@ class App extends Component {
 
 }
 
-export default App;
+const mapStateToProps = state => ({
+  promoCode: state.promoCode.value
+})
+
+export default connect(mapStateToProps, {handleChange}) (App);
---
+++ src/components/PromoCode/PromoCode.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Button, Collapse, Form, Row, Col, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {handleChange} from '../../actions/promoCodeActions';
+
+class PromoCodeDiscount extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false
+        }
+    }
+
+    handleChange = e => {
+        this.props.handleChange(e);
+    };
+
+    render() {
+        return (
+            <div>
+                <Button
+                    className="promo-code-button"
+                    bsStyle="link"
+                    onClick={() => this.setState({ open: !this.state.open })}
+                >
+                    {this.state.open === false ? `Apply ` : `Hide `}
+                    promo code
+                    {this.state.open === false ? ` +` : ` -`}
+                </Button>
+                <Collapse in={this.state.open}>
+                    <Row className="show-grid">
+                        <Col md={12}>
+                            <Form>
+                                <FormGroup controlId="formInLineNumber">
+                                    <FormLabel>Promo Code</FormLabel>
+                                    <FormControl
+                                        type="text"
+                                        placeholder="Enter your promo code"
+                                        value={this.props.promoCode}
+                                        onChange={this.handleChange}
+                                    />
+                                </FormGroup>
+                                <Button
+                                    block
+                                    bsStyle="success"
+                                    className="btn-round"
+                                    disabled={this.props.isDisabled}
+                                    onClick={this.props.giveDiscount}
+                                >
+                                    Apply
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Collapse>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => ({
+    promoCode: state.promoCode.value
+});
+
+export default connect(mapStateToProps, {handleChange}) (PromoCodeDiscount);
