802c14d1224cdd7997bd78f1d88ac7088ea05c60
= <=>
2019-10-03 09:25:18 +0700

    Updates

---
+++ precache-manifest.b5bb5a683be22759a1dbdd6a550e0b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1420a5595bd42da12cc9f96c4cbb5d5f",
+    "url": "/cart-shopping/index.html"
+  },
+  {
+    "revision": "819415aa289f15f3893e",
+    "url": "/cart-shopping/static/css/main.846fb5f3.chunk.css"
+  },
+  {
+    "revision": "8064e36ca986e20cea17",
+    "url": "/cart-shopping/static/js/2.35316e56.chunk.js"
+  },
+  {
+    "revision": "819415aa289f15f3893e",
+    "url": "/cart-shopping/static/js/main.39e181fb.chunk.js"
+  },
+  {
+    "revision": "7ca4a8cc9b4d10a342ad",
+    "url": "/cart-shopping/static/js/runtime-main.6346a9d3.js"
+  }
+]);
